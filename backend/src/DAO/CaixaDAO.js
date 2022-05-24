e0796c44538a889a43d57387e33327c6fe9236e7
marinazeni <marina_zeni@hotmail.com>
2022-05-24 19:09:17 -0300

    AbrirCaixa

---
+++ backend/src/DAO/CaixaDAO.js
@@ -0,0 +1,39 @@
+module.exports = class CaixaDAO{
+
+    async gravar(caixa, db) {
+        let sql = "INSERT INTO caixa (valor, data, status) " +
+            "VALUES (?, ?, ?)"
+        const valores = [caixa.getvalor(), caixa.getdata(), caixa.getstatus()]
+        const result = await db.manipula(sql, valores)
+        return result
+    }
+
+    async alterar(caixa, db) {
+        let sql = "UPDATE caixa SET valor = ?, data = ?, status = ? WHERE id = ?"
+        const valores = [caixa.getvalor(), caixa.getdata(), caixa.getstatus(), caixa.getid()]
+        const result = await db.manipula(sql, valores);
+        return result
+    }
+
+    async excluir(caixa, db) {
+        let sql = "DELETE FROM caixa WHERE id = ?"
+        const valor = [caixa.getid()]
+        const result = await db.manipula(sql, valor)
+        console.log(result)
+        return result
+    }
+
+    async listar(db) {
+        const sql = "SELECT * FROM caixa"
+        const result = await db.consulta(sql, null);   
+        return result;
+    }
+
+    async listarId(id,db){
+        const sql = "SELECT * from caixa where id = ?"
+        const valores = [id]
+        const result = await db.consulta(sql,valores);
+        return result;
+    }
+
+}
---
+++ backend/src/Entidades/Caixa.js
@@ -0,0 +1,73 @@
+const CaixaDAO = require('../DAO/CaixaDAO')
+class Mensalidade {
+
+    constructor(id,valor,data,status){
+        this.id = id;
+        this.valor = valor;
+        this.data = data;
+        this.status=status;
+        this.idEscoteiro=idEscoteiro;
+    }
+
+    getid(){
+        return this.id;
+    }
+
+    setid(novoId){
+        this.id = novoId;
+    }
+
+    getvalor(){
+        return this.valor;
+    }
+
+    setvalor(novoValor){
+        this.valor = novoValor;
+    }
+
+    getdata(){
+        return this.data;
+    }
+
+    setdataPag(novadata){
+        this.data = novadata;
+    }
+
+    getstatus(){
+        return this.status;
+    }
+
+    setstatus(novostatus){
+        this.status = novostatus;
+    }
+
+
+    async gravar(db){
+        const resp=await new CaixaDAO().gravar(this,db);
+        this.id=resp.lastId; 
+    }
+
+    async alterar(db){
+        await new CaixaDAO().alterar(this,db)
+    }
+
+    async excluir(db){
+        await new CaixaDAO().excluir(this,db)
+    }
+
+    async buscarId(id,db){
+        const result = await new CaixaDAO().listarId(id,db)
+        let obj = new Caixa(result.data[i].id, result.data[0].valor, result.data[0].data, result.data[0].status)
+        return obj
+    }
+
+    async listar(db){
+        const result = await new CaixaDAO().listar(db)
+        let lista = []
+        for(let i = 0;i<result.data.length;i++){
+            lista.push(new Caixa(result.data[i].id, result.data[i].valor, result.data[i].data, result.data[i].status))
+        }
+        return lista
+    }
+}
+module.exports = Caixa;
