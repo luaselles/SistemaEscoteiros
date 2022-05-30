de904f1f8e953ba4870bb35aa3a0e6ed34f4e1bd
Rafael Silva Damacena <fael_damacena@hotmail.com>
2022-05-29 23:12:30 -0300

    Commit

--- backend/src/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const routes = require('./routes');
-const app = express();
-
-app.use(express.json());
-app.use(routes);
-app.listen(4000,() =>console.log("servidor rodando na porta 4000"));
