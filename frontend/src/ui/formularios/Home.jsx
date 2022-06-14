a06b9f6e60fba1c5141aa47d43ce733f534a2c71
luaselles <lu.selles@unoeste.edu.br>
2022-06-14 20:19:52 -0300

    boadesistoo

--- frontend/src/ui/formularios/Home.jsx
+++ frontend/src/ui/formularios/Home.jsx
@@ -13,8 +13,10 @@ export default function Home(props){
                 .catch(error => {
                     alert(error)
                 });
+            //setInterval(teste,86400000);
             alert("Caixa aberto (" + data + ")")
             document.getElementById("botao").disabled = true;
+            
         }
     }
 
@@ -23,20 +25,47 @@ export default function Home(props){
         const timeElapsed = Date.now();
         const data = new Date(timeElapsed);
 
-        if (window.confirm("Deseja abrir o caixa?")) {
-            await fetch('http://localhost:4000/abrirCaixa', { method: "POST" })
+        if (window.confirm("Deseja Gerar Mensalidades aos Inscritos?")) {
+            await fetch('http://localhost:4000/gerarmensalidade', { method: "GET" })
                 .then(resposta => resposta.json())
                 .catch(error => {
                     alert(error)
                 });
-            alert("Caixa aberto (" + data + ")")
-            document.getElementById("botao").disabled = true;
+            alert("Mensalidades Geradas (" + data + ")")
+            document.getElementById("botao1").disabled = true;
         }
+    }
+    function teste(){
+        document.getElementById("botao").disabled = false;
+    }
+    async function gerarAutomatico() {
+        window.setInterval(teste,10000)
+        const timeElapsed = Date.now();
+        const data = new Date(timeElapsed);
+        if(data.getDay() === 14 && data.getMonth() === 6 && data.getHours() === 20 && data.getMinutes() === 2){
+            await fetch('http://localhost:4000/gerarmensalidade', { method: "GET" })
+                .then(resposta => resposta.json())
+                .catch(error => {
+                    alert(error)
+                });
+            alert("Mensalidades Geradas (" + data + ") estabelecida")
+            document.getElementById("botao1").disabled = true;
+        }
+        /*if (window.confirm("Deseja Gerar Mensalidades aos Inscritos?")) {
+            await fetch('http://localhost:4000/gerarmensalidade', { method: "GET" })
+                .then(resposta => resposta.json())
+                .catch(error => {
+                    alert(error)
+                });
+            alert("Mensalidades Geradas (" + data + ")")
+            document.getElementById("botao1").disabled = true;
+        }*/
+        console.log(data)
     }
 
     return(
         <div className="cabecalho">
-            <div className="div">
+            <div className="div" onLoad={gerarAutomatico()}>
                     <a className="aa1" href="/eventos">Tabela de Eventos</a>
                     <a className="aa" href="/Produtos">Tabela de Produtos</a>
                     <a className="aa" href="/escoteiros">Tabela de Escoteiros</a>
@@ -44,7 +73,7 @@ export default function Home(props){
                     <button id="botao" className="aa" onClick={() => fetchAbrirCaixa()}>
                         Abrir caixa
                     </button>
-                    <button id="botao" className="aa" onClick={() => gerenciarMensalidades()}>
+                    <button id="botao1" className="aa" onClick={() => gerenciarMensalidades()}>
                         Gerar Mensalidades
                     </button>
             </div>
