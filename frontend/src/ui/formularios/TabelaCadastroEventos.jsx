6ffc3a7aa013207ae5599173f8005c45a4483751
luaselles <lu.selles@unoeste.edu.br>
2023-10-01 15:16:41 -0300

    ajustes

--- frontend/src/ui/formularios/TabelaCadastroEventos.jsx
+++ frontend/src/ui/formularios/TabelaCadastroEventos.jsx
@@ -1,83 +1,103 @@
-import { Button } from "react-bootstrap";
-import { IconeEdicao, IconeExclusao, IconeDinheiro, IconeLista } from "../icones/icones";
+import { Button, Modal, Table } from "react-bootstrap";
+import { useState } from "react";
+
+import { IconeEdicao, IconeExclusao, IconeDinheiro, IconeLista, IconeExibir, IconeExcluir } from "../icones/icones";
 import evento1 from '../../imagens/evento1.png';
 
 export default function TabelaCadastroEventos(props) {
+    const [show, setShow] = useState(false);
+    const [data, setData] = useState({ descricao: '', nome: '' });
+
+    function formatData(data) {
+        const novaData = new Date(data);
+        novaData.toLocaleString('pt-BR');
+        return novaData.getDate() + '/' + novaData.getMonth() + '/' + novaData.getFullYear();
+    }
+
     return (
-        <div class="container mb-2">
-            <div class="mt-2 mb-2">
-                <div class="row">
-                    <div className="col-4">
-                        <figure class="figure">
-                            <img src={evento1} class="figure-img img-fluid rounded" alt="..." />
-                        </figure>
-                    </div>
-                    <div className="col-4">
-                        <figure class="figure">
-                            <img src={evento1} class="figure-img img-fluid rounded" alt="..." />
-                        </figure>
-                    </div>
-                    <div className="col-4">
-                        <figure class="figure">
-                            <img src={evento1} class="figure-img img-fluid rounded" alt="..." />
-                        </figure>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
+        <div className="container mb-2 mt-2">
+            <div className="card">
                 <div className="m-3">
-                    <h1 class="display-6">Informações sobre Eventos</h1>
-                    <p class="lead">
+                    <h1 className="display-6">Informações sobre Eventos</h1>
+                    <p className="lead">
                         Informações, sobre eventos, data endereço entre outros.
                     </p>
                     <hr />
-                    <table class="table table-striped">
-                        <thead>
+                    <div className="row">
+                        {props.eventos.map((evento) => {
+                            return (
+                                <div className="col">
+                                    <div className="card cardEvento" >
+                                        <img src={evento1} className="card-img-top" alt="..." />
+                                        <div className="card-body">
+                                            <h5 className="card-title">{evento.nomeEvent}</h5>
+                                            <p className="card-text">{evento.descricao}</p>
+                                            <div className="btn-group" role="group" aria-label="Basic example">
+                                                <Button onClick={() => {
+                                                    setData({
+                                                        responsavel: evento.respevento,
+                                                        rua: evento.rua,
+                                                        bairro: evento.bairro,
+                                                        cidade: evento.cidade,
+                                                        numero: evento.num,
+                                                        data: formatData(evento.data)
+                                                    })
+                                                    setShow(true)
+                                                }}><IconeExibir /></Button>
+                                                <Button onClick={() => { props.atualizarValor(evento) }}><IconeDinheiro /></Button>{' '}
+                                                <Button onClick={() => { props.mostrarExtrato(evento) }}><IconeLista /></Button>{' '}
+                                                <Button onClick={() => { props.atualizarEvento(evento) }}><IconeEdicao /></Button>{' '}
+                                                <Button onClick={() => { props.deletarEvento(evento) }}><IconeExclusao /></Button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
+            <Modal show={show} onHide={() => setShow(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Detalhes do Evento:</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <table class="table">
+                        <tbody>
                             <tr>
-                                <th scope="col"></th>
-                                <th scope="col">Evento</th>
+                                <th scope="col">Data</th>
                                 <th scope="col">Responsável</th>
-                                <th scope="col">Descrição</th>
+                            </tr>
+                            <tr>
+                                <td>{data.data}</td>
+                                <td>{data.responsavel}</td>
+                            </tr>
+                            <tr>
                                 <th scope="col">Rua</th>
                                 <th scope="col">Bairro</th>
-                                <th scope="col">Cidade</th>
-                                <th scope="col">N°</th>
-                                <th scope="col">Data</th>
-                                <th scope="col"></th>
                             </tr>
-                        </thead>
-                        <tbody>
-                            {props.eventos.map((evento) => {
-                                return (
-                                    <tr key={evento.id}>
-                                        <th>{evento.id}</th>
-                                        <td >{evento.nomeEvent}</td>
-                                        <td>{evento.respevento}</td>
-                                        <td>{evento.descricao}</td>
-                                        <td>{evento.rua}</td>
-                                        <td>{evento.bairro}</td>
-                                        <td>{evento.cidade}</td>
-                                        <td>{evento.num}</td>
-                                        <td>{evento.data}</td>
-                                        <td>
-                                            <div class="btn-group" role="group" aria-label="Basic example">
-                                                <Button variant="outline-success" onClick={() => { props.atualizarValor(evento) }}><IconeDinheiro /></Button>{' '}
-                                                <Button variant="outline-info" onClick={() => { props.mostrarExtrato(evento) }}><IconeLista /></Button>{' '}
-                                                <Button variant="outline-primary" onClick={() => { props.atualizarEvento(evento) }}><IconeEdicao /></Button>{' '}
-                                                <Button variant="outline-danger" onClick={() => { props.deletarEvento(evento) }}><IconeExclusao /></Button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
-
+                            <tr>
+                                <td>{data.rua}</td>
+                                <td>{data.bairro}</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Cidade</th>
+                                <th scope="col">Número</th>
+                            </tr>
+                            <tr>
+                                <td>{data.cidade}</td>
+                                <td>{data.numero}</td>
+                            </tr>
                         </tbody>
                     </table>
-                </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={() => setShow(false)}>
+                        <IconeExcluir />  Fechar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div >
 
-
-            </div>
-
-        </div>
     );
 }
